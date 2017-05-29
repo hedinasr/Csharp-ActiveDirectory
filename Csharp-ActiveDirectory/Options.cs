23dd91fb9a28eec80a21424c84523cf25e406090
Hedi Nasr <h.nsr69@gmail.com>
2017-05-29 04:28:02 +0200

    Populate classes

--- Csharp-ActiveDirectory/Options.cs
+++ Csharp-ActiveDirectory/Options.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommandLine;
+using CommandLine.Text;
 
 namespace Csharp_ActiveDirectory
 {
+    internal abstract class CommonSubOptions
+    {
+        [Option('d', "domain", Required = true, HelpText = "Active Directory domain")]
+        public string Domain { get; set; }
+
+        [Option('u', "username", Required = true, HelpText = "Active Directory username")]
+        public string Username { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "Active Directory password")]
+        public string Password { get; set; }
+
+        [Option('f', "file", Required = true, HelpText = "CSV file (example: -f profs.csv)")]
+        public string File { get; set; }
+
+        [Option('c', "create", HelpText = "Create users")]
+        public bool Create { get; set; }
+
+        [Option('r', "remove", HelpText = "Remove users")]
+        public bool Delete { get; set; }
+
+        [Option("create-directories", HelpText = "Create students home directories")]
+        public bool CreateDirectories { get; set; }
+    }
+
+    internal class StudentsSubOptions : CommonSubOptions
+    {
+        [Option('y', "year", HelpText = "School year (example: -y 2016-2017)")]
+        public string Year { get; set; }
+
+        [Option('a', "archived", HelpText = "Archive the given user (example: -a <username>)")]
+        public string Archived { get; set; }
+    }
+
+    internal class ProfessorsSubOptions : CommonSubOptions
+    {
+    }
+
     class Options
     {
+        [HelpVerbOption]
+        public string GetUsage(string verb)
+        {
+            return HelpText.AutoBuild(this, verb);
+        }
+
+        [VerbOption("students",
+            HelpText = "Create students in Active Directory")]
+        public StudentsSubOptions CreateStudents { get; set; }
+
+        [VerbOption("professors",
+            HelpText = "Create professors in Active Directory")]
+        public ProfessorsSubOptions CreateProfessors { get; set; }
     }
 }
--- Csharp-ActiveDirectory/Program.cs
+++ Csharp-ActiveDirectory/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,62 @@ namespace Csharp_ActiveDirectory
     {
         static void Main(string[] args)
         {
+            string invokeVerb = string.Empty;
+            object invokeVerbInstance = null;
+
+            // Contient toutes les numéros des classes
+            HashSet<string> classes = new HashSet<string>();
+
+            var options = new Options();
+            if (!CommandLine.Parser.Default.ParseArgumentsStrict(args, options,
+                (verb, subOptions) =>
+                {
+                    invokeVerb = verb;
+                    invokeVerbInstance = subOptions;
+                },
+                () => { Console.WriteLine("Example: ADGestion.exe students -d grp05.local -u admin -p admin -f students.csv -y 2016-2017 -c"); }))
+            {
+                Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
+            }
+
+            if (invokeVerb == "students")
+            {
+                var subOptions = (StudentsSubOptions)invokeVerbInstance;
+                Console.WriteLine("Création des étudiants");
+            }
+
+            if (invokeVerb == "professors")
+            {
+                var subOptions = (ProfessorsSubOptions)invokeVerbInstance;
+                Console.WriteLine("Création des professeurs");
+            }
+        }
+
+        static List<string[]> ReadCSV(string filename)
+        {
+            using (TextFieldParser parser = new TextFieldParser(filename))
+            {
+                List<string[]> content = new List<string[]>();
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(";");
+                parser.SetFieldWidths(6);
+                parser.ReadLine(); // on saute la première ligne
+
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    // add fields to the list with blanks removed
+                    content.Add(fields.Where(x => !string.IsNullOrEmpty(x)).ToArray());
+                }
+
+                return content;
+            }
+        }
+
+        static string ConstructUsername(string givenName, string surname)
+        {
+            var username = givenName[0] + surname;
+            return username.ToLower();
         }
     }
 }
